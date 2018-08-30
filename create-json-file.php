62c5a18c04a4f05100429b7ded2e80d0c4ef77ad
Nadia Kerr <12670301+keepsmilyn@users.noreply.github.com>
2018-08-30 13:37:15 +1000

    Add file to github.

---
+++ create-json-file.php
@@ -0,0 +1,105 @@
+<?php
+
+$jsonstart = '[';
+
+
+$string = $string . '{';
+	//UNIQUE ID FOR ACCESSORY GROUP
+	$string = $string . '"id":"1531372338",';			//UNIQUE ID (product ID, ref id, eg: XXXX00001, XXXX000002)
+	//TITLE (displays on web)
+	$string = $string . '"title":"Left Sensor",';
+	//DESCRIPTION (displays on web... can include HTML)
+	$string = $string . '"description":"Select a left sensor for your unit. ",';
+	$string = $string . '"query_type":"category_ids",';
+	//CATEGORY (CAT-NAME REQURIED)
+	//EG: "query_ids":"Surveillance > Cameras > Mobotix > Mobotix Accessories > Sensors Modules > M16 Sensors",	
+	$string = $string . '"query_ids":"Accessories > S16 Sensors",';
+
+	$string = $string . '"default_option_id":"",';				//SAME FOR ALL COMPOSITE PRODUCTS
+	$string = $string . '"thumbnail_id":"",';					//SAME FOR ALL COMPOSITE PRODUCTS
+	$string = $string . '"thumbnail_src":"",';					//SAME FOR ALL COMPOSITE PRODUCTS
+	$string = $string . '"quantity_min":1,';					//SAME FOR ALL COMPOSITE PRODUCTS
+	$string = $string . '"quantity_max":"",';					//SAME FOR ALL COMPOSITE PRODUCTS
+	$string = $string . '"priced_individually":true,';			//SAME FOR ALL COMPOSITE PRODUCTS
+	$string = $string . '"shipped_individually":true,';			//SAME FOR ALL COMPOSITE PRODUCTS
+	$string = $string . '"optional":true,';						//SAME FOR ALL COMPOSITE PRODUCTS
+	$string = $string . '"discount":"",';						//SAME FOR ALL COMPOSITE PRODUCTS
+	$string = $string . '"options_style":"thumbnails",';		//SAME FOR ALL COMPOSITE PRODUCTS
+	$string = $string . '"pagination_style":"classic",';		//SAME FOR ALL COMPOSITE PRODUCTS
+	$string = $string . '"display_prices":"hidden"';			//SAME FOR ALL COMPOSITE PRODUCTS
+
+
+	//NEWLY ADDED IN VERSION: 3.14.0-dev
+	$string = $string . '"show_sorting_options":false,';		//SAME FOR ALL COMPOSITE PRODUCTS
+	$string = $string . '"show_filtering_options":true,'; 		//SAME FOR ALL COMPOSITE PRODUCTS
+	$string = $string . '"product_title_visible":true,';		//SAME FOR ALL COMPOSITE PRODUCTS
+	$string = $string . '"product_descr_visible":true,';		//SAME FOR ALL COMPOSITE PRODUCTS
+	$string = $string . '"product_thumb_visible":true,';		//SAME FOR ALL COMPOSITE PRODUCTS
+	$string = $string . '"product_price_visible":true,';		//SAME FOR ALL COMPOSITE PRODUCTS
+	$string = $string . '"subtotal_visible_product":false,';	//SAME FOR ALL COMPOSITE PRODUCTS
+	$string = $string . '"subtotal_visible_cart":true,';		//SAME FOR ALL COMPOSITE PRODUCTS
+	$string = $string . '"subtotal_visible_orders":true,';		//SAME FOR ALL COMPOSITE PRODUCTS
+
+	//USES FILTER ATTRIBUTES SET ON PRODUCTS IN THE SELECTED CATEGORY. 
+	$string = $string . '"attribute_filters":["Product Colour","Sensor type"]';
+
+
+//IF MORE, USE ONE WITH COMMA
+//$string = $string . '}';
+$string = $string . '},';
+
+
+$string = $string . '{';
+	//UNIQUE ID FOR ACCESSORY GROUP
+	$string = $string . '"id":"1531368130",';			//UNIQUE ID (product ID, ref id, eg: XXXX00001, XXXX000002)
+	//TITLE (display on web)
+	$string = $string . '"title":"Right Sensor",';
+	//DESCRIPTION (displays on web... can include HTML)
+	$string = $string . '"description":"Select a right sensor for your unit. ",';
+	$string = $string . '"query_type":"category_ids",';
+	//CATEGORY (CAT-NAME REQURIED)
+	//EG: "query_ids":"Surveillance > Cameras > Mobotix > Mobotix Accessories > Sensors Modules > M16 Sensors",	
+	$string = $string . '"query_ids":"Accessories > S16 Sensors",';
+
+	$string = $string . '"default_option_id":"",';				//SAME FOR ALL COMPOSITE PRODUCTS
+	$string = $string . '"thumbnail_id":"",';					//SAME FOR ALL COMPOSITE PRODUCTS
+	$string = $string . '"thumbnail_src":"",';					//SAME FOR ALL COMPOSITE PRODUCTS
+	$string = $string . '"quantity_min":1,';					//SAME FOR ALL COMPOSITE PRODUCTS
+	$string = $string . '"quantity_max":"",';					//SAME FOR ALL COMPOSITE PRODUCTS
+	$string = $string . '"priced_individually":true,';			//SAME FOR ALL COMPOSITE PRODUCTS
+	$string = $string . '"shipped_individually":true,';			//SAME FOR ALL COMPOSITE PRODUCTS
+	$string = $string . '"optional":true,';						//SAME FOR ALL COMPOSITE PRODUCTS
+	$string = $string . '"discount":"",';						//SAME FOR ALL COMPOSITE PRODUCTS
+	$string = $string . '"options_style":"thumbnails",';		//SAME FOR ALL COMPOSITE PRODUCTS
+	$string = $string . '"pagination_style":"classic",';		//SAME FOR ALL COMPOSITE PRODUCTS
+	$string = $string . '"display_prices":"hidden"';			//SAME FOR ALL COMPOSITE PRODUCTS
+
+
+	//NEWLY ADDED IN VERSION: 3.14.0-dev
+	$string = $string . '"show_sorting_options":false,';		//SAME FOR ALL COMPOSITE PRODUCTS
+	$string = $string . '"show_filtering_options":true,'; 		//SAME FOR ALL COMPOSITE PRODUCTS
+	$string = $string . '"product_title_visible":true,';		//SAME FOR ALL COMPOSITE PRODUCTS
+	$string = $string . '"product_descr_visible":true,';		//SAME FOR ALL COMPOSITE PRODUCTS
+	$string = $string . '"product_thumb_visible":true,';		//SAME FOR ALL COMPOSITE PRODUCTS
+	$string = $string . '"product_price_visible":true,';		//SAME FOR ALL COMPOSITE PRODUCTS
+	$string = $string . '"subtotal_visible_product":false,';	//SAME FOR ALL COMPOSITE PRODUCTS
+	$string = $string . '"subtotal_visible_cart":true,';		//SAME FOR ALL COMPOSITE PRODUCTS
+	$string = $string . '"subtotal_visible_orders":true,';		//SAME FOR ALL COMPOSITE PRODUCTS
+
+	//USES FILTER ATTRIBUTES SET ON PRODUCTS IN THE SELECTED CATEGORY. 
+	$string = $string . '"attribute_filters":["Product Colour","Sensor type"]';
+
+//IF MORE, USE ONE WITH COMMA
+$string = $string . '}';
+//$string = $string . '},';
+
+
+
+$jsonend = ']';
+
+echo $jsonstart.$string.$jsonend;
+
+
+
+
+?>
