d203402cf9180adf821705a4723fffdb352154eb
Nadia Kerr <12670301+keepsmilyn@users.noreply.github.com>
2018-08-30 15:35:52 +1000

    Update create-json-file.php

--- create-json-file.php
+++ create-json-file.php
@@ -1,81 +1,93 @@
-<?php
-$jsonstart = '[';
-$string .=  '{';
-	//UNIQUE ID FOR ACCESSORY GROUP
-	$string .=  '"id":"1531372338",';			//UNIQUE ID (product ID, ref id, eg: XXXX00001, XXXX000002)
-	//TITLE (displays on web)
-	$string .=  '"title":"Accessory Title",';
-	//DESCRIPTION (displays on web... can include HTML)
-	$string .=  '"description":"Select an accessory. ",';
-	$string .=  '"query_type":"category_ids",';
-	//CATEGORY (CAT-NAME REQURIED)
-	//EG: "query_ids":"Category > Subcategory > Subsubcategory > Subsubsubcategory > Subsubsubsubcategory > Subsubsubsubcategory",	
-	$string .=  '"query_ids":"Category > Subcategory",';
-	$string .=  '"default_option_id":"",';				//SAME FOR ALL COMPOSITE PRODUCTS
-	$string .=  '"thumbnail_id":"",';				//SAME FOR ALL COMPOSITE PRODUCTS
-	$string .=  '"thumbnail_src":"",';				//SAME FOR ALL COMPOSITE PRODUCTS
-	$string .=  '"quantity_min":1,';				//SAME FOR ALL COMPOSITE PRODUCTS
-	$string .=  '"quantity_max":"",';				//SAME FOR ALL COMPOSITE PRODUCTS
-	$string .=  '"priced_individually":true,';			//SAME FOR ALL COMPOSITE PRODUCTS
-	$string .=  '"shipped_individually":true,';			//SAME FOR ALL COMPOSITE PRODUCTS
-	$string .=  '"optional":true,';					//SAME FOR ALL COMPOSITE PRODUCTS
-	$string .=  '"discount":"",';					//SAME FOR ALL COMPOSITE PRODUCTS
-	$string .=  '"options_style":"thumbnails",';			//SAME FOR ALL COMPOSITE PRODUCTS
-	$string .=  '"pagination_style":"classic",';			//SAME FOR ALL COMPOSITE PRODUCTS
-	$string .=  '"display_prices":"hidden"';			//SAME FOR ALL COMPOSITE PRODUCTS
-	//NEWLY ADDED IN VERSION: 3.14.0-dev
-	$string .=  '"show_sorting_options":false,';			//SAME FOR ALL COMPOSITE PRODUCTS
-	$string .=  '"show_filtering_options":true,'; 			//SAME FOR ALL COMPOSITE PRODUCTS
-	$string .=  '"product_title_visible":true,';			//SAME FOR ALL COMPOSITE PRODUCTS
-	$string .=  '"product_descr_visible":true,';			//SAME FOR ALL COMPOSITE PRODUCTS
-	$string .=  '"product_thumb_visible":true,';			//SAME FOR ALL COMPOSITE PRODUCTS
-	$string .=  '"product_price_visible":true,';			//SAME FOR ALL COMPOSITE PRODUCTS
-	$string .=  '"subtotal_visible_product":false,';		//SAME FOR ALL COMPOSITE PRODUCTS
-	$string .=  '"subtotal_visible_cart":true,';			//SAME FOR ALL COMPOSITE PRODUCTS
-	$string .=  '"subtotal_visible_orders":true,';			//SAME FOR ALL COMPOSITE PRODUCTS
-	//USES FILTER ATTRIBUTES SET ON PRODUCTS IN THE SELECTED CATEGORY. 
-	$string .=  '"attribute_filters":["Product Colour","Product type"]';
-//IF MORE, USE ONE WITH COMMA
-//$string .=  '}';
-$string .=  '},';
-$string .=  '{';
-	//UNIQUE ID FOR ACCESSORY GROUP
-	$string .=  '"id":"1531368130",';			//UNIQUE ID (product ID, ref id, eg: XXXX00001, XXXX000002)
-	//TITLE (display on web)
-	$string .=  '"title":"Accessory Title",';
-	//DESCRIPTION (displays on web... can include HTML)
-	$string .=  '"description":"Select an accessory. ",';
-	$string .=  '"query_type":"category_ids",';
-	//CATEGORY (CAT-NAME REQURIED)
-	//EG: "query_ids":"Category > Subcategory > Subsubcategory > Subsubsubcategory > Subsubsubsubcategory > Subsubsubsubcategory",	
-	$string .=  '"query_ids":"Category > Subcategory",';
-	$string .=  '"default_option_id":"",';				//SAME FOR ALL COMPOSITE PRODUCTS
-	$string .=  '"thumbnail_id":"",';				//SAME FOR ALL COMPOSITE PRODUCTS
-	$string .=  '"thumbnail_src":"",';				//SAME FOR ALL COMPOSITE PRODUCTS
-	$string .=  '"quantity_min":1,';				//SAME FOR ALL COMPOSITE PRODUCTS
-	$string .=  '"quantity_max":"",';				//SAME FOR ALL COMPOSITE PRODUCTS
-	$string .=  '"priced_individually":true,';			//SAME FOR ALL COMPOSITE PRODUCTS
-	$string .=  '"shipped_individually":true,';			//SAME FOR ALL COMPOSITE PRODUCTS
-	$string .=  '"optional":true,';					//SAME FOR ALL COMPOSITE PRODUCTS
-	$string .=  '"discount":"",';					//SAME FOR ALL COMPOSITE PRODUCTS
-	$string .=  '"options_style":"thumbnails",';			//SAME FOR ALL COMPOSITE PRODUCTS
-	$string .=  '"pagination_style":"classic",';			//SAME FOR ALL COMPOSITE PRODUCTS
-	$string .=  '"display_prices":"hidden"';			//SAME FOR ALL COMPOSITE PRODUCTS
-	//NEWLY ADDED IN VERSION: 3.14.0-dev
-	$string .=  '"show_sorting_options":false,';			//SAME FOR ALL COMPOSITE PRODUCTS
-	$string .=  '"show_filtering_options":true,'; 			//SAME FOR ALL COMPOSITE PRODUCTS
-	$string .=  '"product_title_visible":true,';			//SAME FOR ALL COMPOSITE PRODUCTS
-	$string .=  '"product_descr_visible":true,';			//SAME FOR ALL COMPOSITE PRODUCTS
-	$string .=  '"product_thumb_visible":true,';			//SAME FOR ALL COMPOSITE PRODUCTS
-	$string .=  '"product_price_visible":true,';			//SAME FOR ALL COMPOSITE PRODUCTS
-	$string .=  '"subtotal_visible_product":false,';		//SAME FOR ALL COMPOSITE PRODUCTS
-	$string .=  '"subtotal_visible_cart":true,';			//SAME FOR ALL COMPOSITE PRODUCTS
-	$string .=  '"subtotal_visible_orders":true,';			//SAME FOR ALL COMPOSITE PRODUCTS
-	//USES FILTER ATTRIBUTES SET ON PRODUCTS IN THE SELECTED CATEGORY. 
-	$string .=  '"attribute_filters":["Product Colour","Product type"]';
-//IF MORE, USE ONE WITH COMMA
-$string .=  '}';
-//$string .=  '},';
-$jsonend = ']';
-echo $jsonstart.$string.$jsonend;
+<?php 
+
+$product_array = array(
+	array( 
+		//UNIQUE ID FOR ACCESSORY GROUP	
+		'id' => '1531372338',				//UNIQUE ID (product ID, ref id, eg: XXXX00001, XXXX000002)
+		
+		//TITLE (displays on web)	
+		'title' => 'Accessory Title',
+		
+		//DESCRIPTION (displays on web... can include HTML)	
+		'description' => 'Select an accessory. ',
+		'query_type' => 'category_ids',
+		
+		//CATEGORY (CAT-NAME REQURIED)
+		//EG: "query_ids":"Category > Subcategory > Subsubcategory > Subsubsubcategory > Subsubsubsubcategory > Subsubsubsubcategory",	
+		'query_ids' => 'Category > Subcategory',
+		'default_option_id' => '',			
+		'thumbnail_id' => '',				
+		'thumbnail_src' => '',				
+		'quantity_min' =>1,
+		'quantity_max' => '',				
+		'priced_individually' =>true,		
+		'shipped_individually' =>true,
+		'optional' =>true,			
+		'discount' => '',			
+		'options_style' => 'thumbnails',	
+		'pagination_style' => 'classic',
+		'display_prices' => 'hidden', 	
+		
+		//NEWLY ADDED IN VERSION: 3.14.0-dev	
+		'show_sorting_options' =>false,			
+		'show_filtering_options' =>true,
+		'product_title_visible' =>true,			
+		'product_descr_visible' =>true,			
+		'product_thumb_visible' =>true,			
+		'product_price_visible' =>true,			
+		'subtotal_visible_product' =>false,		
+		'subtotal_visible_cart' =>true,			
+		'subtotal_visible_orders' =>true,		
+		
+		//USES FILTER ATTRIBUTES SET ON PRODUCTS IN THE SELECTED CATEGORY. 	
+		'attribute_filters' => array( 'Product Colour','Product type' )
+	),
+	array( 
+		//UNIQUE ID FOR ACCESSORY GROUP	
+		'id' => '1531372338',				//UNIQUE ID (product ID, ref id, eg: XXXX00001, XXXX000002)
+		
+		//TITLE (displays on web)	
+		'title' => 'Accessory Title',
+		
+		//DESCRIPTION (displays on web... can include HTML)	
+		'description' => 'Select an accessory. ',
+		'query_type' => 'category_ids',
+		
+		//CATEGORY (CAT-NAME REQURIED)
+		//EG: "query_ids":"Category > Subcategory > Subsubcategory > Subsubsubcategory > Subsubsubsubcategory > Subsubsubsubcategory",	
+		'query_ids' => 'Category > Subcategory',
+		'default_option_id' => '',			
+		'thumbnail_id' => '',				
+		'thumbnail_src' => '',				
+		'quantity_min' =>1,
+		'quantity_max' => '',				
+		'priced_individually' =>true,		
+		'shipped_individually' =>true,
+		'optional' =>true,			
+		'discount' => '',			
+		'options_style' => 'thumbnails',	
+		'pagination_style' => 'classic',
+		'display_prices' => 'hidden', 	
+		
+		//NEWLY ADDED IN VERSION: 3.14.0-dev	
+		'show_sorting_options' =>false,			
+		'show_filtering_options' =>true,
+		'product_title_visible' =>true,			
+		'product_descr_visible' =>true,			
+		'product_thumb_visible' =>true,			
+		'product_price_visible' =>true,			
+		'subtotal_visible_product' =>false,		
+		'subtotal_visible_cart' =>true,			
+		'subtotal_visible_orders' =>true,
+		
+		//USES FILTER ATTRIBUTES SET ON PRODUCTS IN THE SELECTED CATEGORY. 	
+		'attribute_filters' => array( 'Product Colour','Product type' )
+	)
+); 
+
+
+$json_products = json_encode( $product_array ); 
+
+echo $json_products;
+
 ?>
